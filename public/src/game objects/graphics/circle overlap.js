01d68652a1274b3b846de94f507aa1d9949351a1
Jameson <jamesonnash@gmail.com>
2021-01-21 11:16:02 +1300

    Changes global variables to class properties.

--- public/src/game objects/graphics/circle overlap.js
+++ public/src/game objects/graphics/circle overlap.js
@@ -1,13 +1,16 @@
-let t = 0;
-let graphics1;
-let graphics2;
-
 class Example extends Phaser.Scene
 {
+    constructor ()
+    {
+        super();
+        this.t = 0;
+        this.graphics1 = undefined;
+        this.graphics2 = undefined;
+    }
     create ()
     {
-        graphics2 = this.add.graphics({x: -16, y: 0}).lineStyle(28, 0x00ffff, 0.8);
-        graphics1 = this.add.graphics().lineStyle(28, 0x0000ff, 0.8);
+        this.graphics2 = this.add.graphics({x: -16, y: 0}).lineStyle(28, 0x00ffff, 0.8);
+        this.graphics1 = this.add.graphics().lineStyle(28, 0x0000ff, 0.8);
 
         //  Create the circles
 
@@ -16,8 +19,8 @@ class Example extends Phaser.Scene
 
         for (let i = 0; i < 8; i++)
         {
-            graphics1.strokeCircle(400, 300, radius1);
-            graphics2.strokeCircle(400, 300, radius2);
+            this.graphics1.strokeCircle(400, 300, radius1);
+            this.graphics2.strokeCircle(400, 300, radius2);
 
             radius1 += 64;
             radius2 += 64;
@@ -27,13 +30,13 @@ class Example extends Phaser.Scene
 
     update ()
     {
-        t += 0.1;
+        this.t += 0.1;
 
-        graphics1.x += Math.sin(t) * 2;
-        graphics1.y += Math.cos(t) * 2;
+        this.graphics1.x += Math.sin(this.t) * 2;
+        this.graphics1.y += Math.cos(this.t) * 2;
 
-        graphics2.x += Math.sin(t) * 3;
-        graphics2.y += Math.cos(t) * 3;
+        this.graphics2.x += Math.sin(this.t) * 3;
+        this.graphics2.y += Math.cos(this.t) * 3;
 
     }
 }
