6c9d018d92f553f39b85e9312e9cf2b42d37ac5c
Heihei03 <22041201+Heihei03@users.noreply.github.com>
2021-01-22 10:17:06 +1300

    Update multiple sprites.js

--- public/src/game objects/sprites/multiple sprites.js
+++ public/src/game objects/sprites/multiple sprites.js
@@ -1,22 +1,27 @@
-class Example extends Phaser.scene {
-    constructor () {
+class Example extends Phaser.scene
+{
+    constructor ()
+    {
         super();
     }
 
-    preload () {
+    preload ()
+    {
         this.load.image('beball', 'assets/sprites/beball1.png');
         this.load.image('atari', 'assets/sprites/atari400.png');
         this.load.image('bikkuriman', 'assets/sprites/bikkuriman.png');
     }
 
-    create () {
+    create ()
+    {
         this.add.sprite(200, 300, 'beball');
         this.add.sprite(500, 300, 'atari');
         this.add.sprite(800, 300, 'bikkuriman');
     }
 }
 
-const config = {
+const config =
+{
     type: Phaser.AUTO,
     width: 800,
     height: 600,
