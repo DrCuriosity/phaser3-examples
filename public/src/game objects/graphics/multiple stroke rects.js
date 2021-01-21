efb7b7d208d5a367118d965b484a023f896f5550
Alex Gedye <AlexSged22@gmail.com>
2021-01-21 16:09:23 +1300

    Updated to Es6 Format

--- public/src/game objects/graphics/multiple stroke rects.js
+++ public/src/game objects/graphics/multiple stroke rects.js
@@ -1,85 +1,81 @@
-var config = {
+class MultipleStrokeRects extends Phaser.Scene {
+
+    constructor () {
+        super();
+        this.start = undefined;
+        this.hsv = undefined;
+        this.graphics = undefined;
+        this.size = 64 * 2;
+        this.historyX = Array(this.size);
+        this.historyY = Array(this.size);
+        this.index = 0;
+    }
+
+    create () {
+        this.graphics = this.add.graphics();
+
+        //  diamond shape from 400x300 =
+
+        //  400 x 100 (top middle)
+        //  100 x 300 (left)
+        //  700 x 300 (right)
+        //  400 x 500 (bottom)
+
+        this.start = new Phaser.Geom.Point(400, 100);
+
+        this.top = new Phaser.Geom.Point(400, 100);
+        this.left = new Phaser.Geom.Point(100, 300);
+        this.right = new Phaser.Geom.Point(700, 300);
+        this. bottom = new Phaser.Geom.Point(400, 500);
+
+        this.hsv = Phaser.Display.Color.HSVColorWheel();
+
+        this.tweens.timeline({
+
+            targets: this.start,
+            loop: -1,
+            duration: 2000,
+
+            tweens: [
+                { x: this.left.x, y: this.left.y, ease: 'Sine.easeOut' },
+                { x: this.bottom.x, y: this.bottom.y, ease: 'Sine.easeInOut' },
+                { x: this.right.x, y: this.right.y, ease: 'Sine.easeInOut' },
+                { x: this.top.x, y: this.top.y, ease: 'Sine.easeIn' }
+            ]
+        });
+
+        //  Fill the history array
+
+        for (var i = 0; i < this.size; i++) {
+        this.historyX[i] = this.start.x;
+        this.historyY[i] = this.start.y;
+        }
+    }
+
+    update () {
+        this.historyX[this.index] = this.start.x;
+        this.historyY[this.index] = this.start.y;
+
+        this.index++;
+
+        if (this.index === this.size) {
+            this.index = 0;
+        }
+
+        this.graphics.clear();
+
+        for (var i = 0; i < 64; i++) {
+            this.graphics.fillStyle(this.hsv[i * 2].color, 1);
+            this.graphics.fillCircle(this.historyX[i * 2], this.historyY[i * 2], 64);
+        }
+
+    }
+}
+const config = {
     width: 800,
     height: 600,
     type: Phaser.CANVAS,
     parent: 'phaser-example',
-    scene: {
-        create: create,
-        update: update
-    }
+    scene: [MultipleStrokeRects]
 };
-
-var game = new Phaser.Game(config);
-
-var start;
-var hsv;
-var graphics;
-var size = 64 * 2;
-var historyX = Array(size);
-var historyY = Array(size);
-var index = 0;
-
-function create ()
-{
-    graphics = this.add.graphics();
-
-    //  diamond shape from 400x300 =
-
-    //  400 x 100 (top middle)
-    //  100 x 300 (left)
-    //  700 x 300 (right)
-    //  400 x 500 (bottom)
-
-    start = new Phaser.Geom.Point(400, 100);
-
-    var top = new Phaser.Geom.Point(400, 100);
-    var left = new Phaser.Geom.Point(100, 300);
-    var right = new Phaser.Geom.Point(700, 300);
-    var bottom = new Phaser.Geom.Point(400, 500);
-
-    hsv = Phaser.Display.Color.HSVColorWheel();
-
-    this.tweens.timeline({
-
-        targets: start,
-        loop: -1,
-        duration: 2000,
-
-        tweens: [
-            { x: left.x, y: left.y, ease: 'Sine.easeOut' },
-            { x: bottom.x, y: bottom.y, ease: 'Sine.easeInOut' },
-            { x: right.x, y: right.y, ease: 'Sine.easeInOut' },
-            { x: top.x, y: top.y, ease: 'Sine.easeIn' }
-        ]
-
-    });
-
-    //  Fill the history array
-    for (var i = 0; i < size; i++)
-    {
-        historyX[i] = start.x;
-        historyY[i] = start.y;
-    }
-}
-
-function update ()
-{
-    historyX[index] = start.x;
-    historyY[index] = start.y;
-
-    index++;
-
-    if (index === size)
-    {
-        index = 0;
-    }
-
-    graphics.clear();
-
-    for (var i = 0; i < 64; i++)
-    {
-        graphics.fillStyle(hsv[i * 2].color, 1);
-        graphics.fillCircle(historyX[i * 2], historyY[i * 2], 64);
-    }
-
-}
+const game = new Phaser.Game(config);
