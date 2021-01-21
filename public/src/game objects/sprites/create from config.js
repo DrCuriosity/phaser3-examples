e8246d3588733acbfc5c7556a042f41903636d8c
Alex Gedye <AlexSged22@gmail.com>
2021-01-21 16:00:23 +1300

    updated to es6

--- public/src/game objects/sprites/create from config.js
+++ public/src/game objects/sprites/create from config.js
@@ -1,79 +1,86 @@
-class CreatreFromConfig extends Phaser.Scene
-{
-     preload ()
-{
-    this.load.image('bunny', 'assets/sprites/bunny.png');
-    this.load.image('atari', 'assets/sprites/atari400.png');
-    this.load.image('logo', 'assets/sprites/phaser2.png');
-}
+class CreatreFromConfig extends Phaser.Scene {
 
-     create ()
-{
-    //  Implicit values
-    var config1 = {
-        key: 'bunny',
-        x: 400,
-        y: 300
-    };
+    preload () {
+        this.load.image('bunny', 'assets/sprites/bunny.png');
+        this.load.image('atari', 'assets/sprites/atari400.png');
+        this.load.image('logo', 'assets/sprites/phaser2.png');
+    }
 
-    //  Implicit values
-    var config2 = {
-        key: 'bunny',
-        x: 400,
-        y: 300,
-        flipX: true,
-        flipY: true
-    };
+    create () {
 
-    //  An array means 'pick one element at random'
-    var config3 = {
-        key: [ 'bunny', 'atari', 'logo' ],
-        x: 400,
-        y: 300
-    };
+        //  Implicit values
 
-    //  randInt means pick a random integer between the min and max values given
-    var config4 = {
-        key: 'atari',
-        x: { randInt: [ 0, 800 ] },
-        y: 300
-    };
+        this.config1 = {
+            key: 'bunny',
+            x: 400,
+            y: 300
+        };
 
-    //  randFloat means pick a random float between the min and max values given
-    var config5 = {
-        key: 'logo',
-        x: 400,
-        y: 300,
-        alpha: { randFloat: [ 0.1, 1 ] }
-    };
+        //  Implicit values
 
-    //  If the property is a function it will invoke it and use the result as the value
-    var config6 = {
-        key: 'atari',
-        x: 400,
-        y: function () {
-            return 100 + Math.random() * 500;
-        }
-    };
+        this.config2 = {
+            key: 'bunny',
+            x: 400,
+            y: 300,
+            flipX: true,
+            flipY: true
+        };
 
-    //  An example using randFloat to set the scale of a Sprite
-    var config7 = {
-        key: 'logo',
-        x: 400,
-        y: 300,
-        scale: { randFloat: [ 0.2, 2 ] }
-    };
+        //  An array means 'pick one element at random'
 
-    //  An example using randFloat to set independent x and y scale values
-    var config8 = {
-        key: 'logo',
-        x: 400,
-        y: 300,
-        scale: { x: { randFloat: [ 0.2, 2 ] }, y: { randFloat: [ 1.5, 3 ] } }
-    };
+        this.config3 = {
+            key: [ 'bunny', 'atari', 'logo' ],
+            x: 400,
+            y: 300
+        };
 
-    this.make.sprite(config8);
-  }
+        //  randInt means pick a random integer between the min and max values given
+
+        this.config4 = {
+            key: 'atari',
+            x: { randInt: [ 0, 800 ] },
+            y: 300
+        };
+
+        //  randFloat means pick a random float between the min and max values given
+
+        this.config5 = {
+            key: 'logo',
+            x: 400,
+            y: 300,
+            alpha: { randFloat: [ 0.1, 1 ] }
+        };
+
+        //  If the property is a function it will invoke it and use the result as the value
+
+        this.config6 = {
+            key: 'atari',
+            x: 400,
+            y: function () {
+                return 100 + Math.random() * 500;
+            }
+        };
+
+        //  An example using randFloat to set the scale of a Sprite
+
+        this.config7 = {
+            key: 'logo',
+            x: 400,
+            y: 300,
+            scale: { randFloat: [ 0.2, 2 ] }
+        };
+
+        //  An example using randFloat to set independent x and y scale values
+
+        this.config8 = {
+            key: 'logo',
+            x: 400,
+            y: 300,
+            scale: { x: { randFloat: [ 0.2, 2 ] }, y: { randFloat: [ 1.5, 3 ] } }
+        };
+
+        this.make.sprite(this.config8);
+    }
 }
 const config = {
     type: Phaser.WEBGL,
@@ -81,6 +88,6 @@ const config = {
     width: 800,
     height: 600,
     scene: [CreatreFromConfig]
-};
+    };
 
 const game = new Phaser.Game(config);
